faf93f0599b0195aa320ba36a6ff9119ae53dad0
Nicky Kwan <nickykwn@gmail.com>
2017-08-26 15:16:27 -0400

    added services directory

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const dotEnv = require('dotenv').config({silent: true});
-const express = require('express');
-const logger = require('morgan');
-const path = require('path');
-const bodyParsey = require('body-parser');
+const dotEnv        = require('dotenv').config({silent: true});
+const express       = require('express');
+const logger        = require('morgan');
+const path          = require('path');
+const bodyParser    = require('body-parser');
 
-const app = express();
-const port = process.argv[2] || process.env.PORT || 3000;
+const app           = express();
+const port          = process.argv[2] || process.env.PORT || 3000;
 
 app.use(logger('dev'));
 
